57b2b361fd8650d582cf43664fe2418fbefc39b4
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-08 17:01:00 +0000

    msm8953-common: libhidl: build a system version of android.hidl.base@1.0
    Change-Id: Icf4a29d6f8ef1517f4aa1971fc80a748632d8068
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: AlexThundrous <alexthundrous2104@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -194,6 +194,7 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_vendor \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     libhidltransport
 
