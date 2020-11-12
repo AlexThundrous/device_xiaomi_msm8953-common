nOUr-eldien/android_device_xiaomi_ysl

ysl: init: Enable config_avoidGfxAccel for 2/3GB variants
- This flag turns off GPU/CPU intensive UI effects and
  reduces the RAM footprint.

- Using this is a better compromise than the 'low_ram' flag
  as this does not disable features in the rom but still
  reduces RAM usage and gives better smoothness in the UI.

Co-authored-by: Jyotiraditya <jyotiraditya@aospa.co>

init/init_msm8953.cpp
@@ -54,10 +54,20 @@ void check_device()
         heapminfree = "2m";
    }
 }
+void set_avoid_gfxaccel_config() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram <= 3072ull * 1024 * 1024) {
+        // Reduce memory footprint
+        property_set("ro.config.avoid_gfx_accel", "true");
+    }
+}
 
 void vendor_load_properties()
 {
     check_device();
+	set_avoid_gfxaccel_config();
 
     property_set("dalvik.vm.heapstartsize", "16m");
     property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);

AlexThundrous/device_xiaomi_msm8953-common

Signed-off-by: Alex Thundrous <alexthundrous2104@gmail.com>

init/init_msm8953.cpp
@@ -94,10 +94,20 @@ void property_override(char const prop[], char const value[], bool add = true)
         __system_property_add(prop, strlen(prop), value, strlen(value));
     }
 }
+void set_avoid_gfxaccel_config() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram <= 3072ull * 1024 * 1024) {
+        // Reduce memory footprint
+        SetProperty("ro.config.avoid_gfx_accel", "true");
+    }
+}
 
 void vendor_load_properties()
 {
     check_device();
+	set_avoid_gfxaccel_config();
 
     property_override("dalvik.vm.heapstartsize", heapstartsize);
     property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);