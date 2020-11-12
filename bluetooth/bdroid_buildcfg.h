56a206a963384d9f17607b72219b89637903ef28
SunnyRaj84348 <SunnyRaj84348@users.noreply.github.com>
2020-11-12 20:20:05 +0530

    msm8953-common: Tweaks For Better Ram Management
    Signed-off-by: Alex Thundrous <alexthundrous2104@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,8 +19,9 @@
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
