392bd08510ad97aa2c30712ccd3f27a10e286d64
dianlujitao <dianlujitao@lineageos.org>
2020-11-21 07:04:10 +0000

    msm8953-common: Build vendor android.hidl.base@1.0.so
    Signed-off-by: AlexThundrous <alexthundrous2104@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -192,6 +192,7 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_vendor \
     android.hidl.manager@1.0 \
     libhidltransport
 
