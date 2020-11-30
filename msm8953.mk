6894fd7bf758709445774889ef7f99e51f7c4c60
AlexThundrous <alexthundrous2104@gmail.com>
2020-11-30 12:04:31 +0530

    msm8953-common: Remove android.base.hidl
    Signed-off-by: AlexThundrous <alexthundrous2104@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -194,9 +194,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
     libhidltransport \
     libhidltransport.vendor
 
